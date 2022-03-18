2c435548f21fe878254e8c0d0a974e12d4ce5a57
CostaAspro708 <n10464174@qut.edu.au>
2022-03-18 14:34:36 +1000

    Added instructions to header

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
     <div class="header">
       <h1>Marvel Dream Team Selector</h1>
-      <p>Select characters by clicking the position container then clicking the name of the character on the left</p>
+      <p>Select characters by clicking the position container on the football field then clicking the name of the character on the left</p>
     </div>
       <div>
         <CharacterSearch/>
