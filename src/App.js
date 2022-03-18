4d3d69947d194bb17a480b04cf49cfaa7b336c95
CostaAspro708 <n10464174@qut.edu.au>
2022-03-18 15:09:13 +1000

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
     <div class="header">
       <h1>Marvel Dream Team Selector</h1>
-      <p>Select characters by clicking the position container on the football field then clicking the name of the character on the left</p>
+      <p>Select characters by clicking the containers on the football field then clicking the name of the character on the right.</p>
     </div>
       <div>
         <CharacterSearch/>
